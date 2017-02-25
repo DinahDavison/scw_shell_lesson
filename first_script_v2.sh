7d445204b34d0fc605698063b1595405a02ba7ad
Dinah Davison <Dinah Davison>
2017-02-25 12:31:22 -0700

    Added help

--- first_script_v2.sh
+++ first_script_v2.sh
@@ -1,2 +1,8 @@
-# This script is used to count the number of characters in a file. 
+#If the first argument is -h, then show the user how to use the script. Echo is a command to display comment.
+if [ "$1" == "--h" ] || [ "$1" == "--help" ]; then
+	echo "Usage: sh $0 input_file output_file"
+	exit
+fi
+
+ #This script is used to count the number of characters in a file. 
 head -5 $1 | sort | wc -c > $2 
